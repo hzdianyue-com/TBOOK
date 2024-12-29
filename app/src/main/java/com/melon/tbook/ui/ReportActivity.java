98d2a6a9eb38ea24da514cad81c28c97017d8faf
isold23 <isold.wang@gmail.com>
2024-12-29 19:20:01 +0800

    Add year&month select

--- app/src/main/java/com/melon/tbook/ui/ReportActivity.java
+++ app/src/main/java/com/melon/tbook/ui/ReportActivity.java
@@ -2,7 +2,10 @@ package com.melon.tbook.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +15,6 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
-
 import com.melon.tbook.R;
 import com.melon.tbook.db.DBHelper;
 import com.melon.tbook.db.Record;
@@ -33,9 +35,11 @@ public class ReportActivity extends AppCompatActivity {
     private DBHelper dbHelper;
     private TextView reportTitleTextView;
     private TextView noDataTextView;
-    private  SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
-    private SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
+    private Spinner monthSpinner, yearSpinner;
+    private int selectMonth, selectYear;
 
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
+    private SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,21 +50,84 @@ public class ReportActivity extends AppCompatActivity {
         pieChart = findViewById(R.id.pie_chart);
         reportTitleTextView = findViewById(R.id.report_title);
         noDataTextView = findViewById(R.id.report_no_data);
+        monthSpinner = findViewById(R.id.month_spinner);
+        yearSpinner = findViewById(R.id.year_spinner);
 
         dbHelper = new DBHelper(this);
         monthlyReportButton.setOnClickListener(v -> generateMonthlyReport());
         yearlyReportButton.setOnClickListener(v -> generateYearlyReport());
 
+
+        initYearSpinner();
+
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectYear = Integer.parseInt(parent.getItemAtPosition(position).toString());
+                initMonthSpinner(selectYear);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectMonth = position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         generateMonthlyReport();
     }
 
-    private void generateMonthlyReport() {
+    private void initYearSpinner() {
         Calendar calendar = Calendar.getInstance();
-        Date now = calendar.getTime();
-        String month = dateFormat.format(now);
+        int currentYear = calendar.get(Calendar.YEAR);
+
+        List<String> yearList = new ArrayList<>();
+        for (int i = currentYear - 10; i <= currentYear + 10; i++) {
+            yearList.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, yearList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(adapter);
+        int index = yearList.indexOf(String.valueOf(currentYear));
+        yearSpinner.setSelection(index);
+        selectYear = currentYear;
+        initMonthSpinner(currentYear);
+
+    }
+
+
+    private void initMonthSpinner(int year){
+        monthSpinner.setVisibility(View.VISIBLE);
+        List<String> monthList = new ArrayList<>();
+        for(int i = 1; i <= 12; i++){
+            monthList.add(String.format("%02d",i));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, monthList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(adapter);
+        Calendar calendar = Calendar.getInstance();
+        int index = calendar.get(Calendar.MONTH);
+        monthSpinner.setSelection(index);
+        selectMonth = index;
+    }
+
+
+    private void generateMonthlyReport() {
+        String month =  String.format("%d-%02d",selectYear,selectMonth + 1);
         reportTitleTextView.setText(month + "  报表");
 
         Calendar startCal = Calendar.getInstance();
+        startCal.set(Calendar.YEAR,selectYear);
+        startCal.set(Calendar.MONTH,selectMonth);
         startCal.set(Calendar.DAY_OF_MONTH,1);
         startCal.set(Calendar.HOUR_OF_DAY,0);
         startCal.set(Calendar.MINUTE,0);
@@ -68,6 +135,8 @@ public class ReportActivity extends AppCompatActivity {
         Date startDate = startCal.getTime();
 
         Calendar endCal = Calendar.getInstance();
+        endCal.set(Calendar.YEAR,selectYear);
+        endCal.set(Calendar.MONTH,selectMonth);
         endCal.set(Calendar.DAY_OF_MONTH,endCal.getActualMaximum(Calendar.DAY_OF_MONTH));
         endCal.set(Calendar.HOUR_OF_DAY,23);
         endCal.set(Calendar.MINUTE,59);
@@ -78,11 +147,10 @@ public class ReportActivity extends AppCompatActivity {
     }
 
     private void generateYearlyReport() {
-        Calendar calendar = Calendar.getInstance();
-        Date now = calendar.getTime();
-        String year = yearFormat.format(now);
+        String year = String.valueOf(selectYear);
         reportTitleTextView.setText(year + "  报表");
         Calendar startCal = Calendar.getInstance();
+        startCal.set(Calendar.YEAR,selectYear);
         startCal.set(Calendar.MONTH,0);
         startCal.set(Calendar.DAY_OF_MONTH,1);
         startCal.set(Calendar.HOUR_OF_DAY,0);
@@ -92,6 +160,7 @@ public class ReportActivity extends AppCompatActivity {
 
 
         Calendar endCal = Calendar.getInstance();
+        endCal.set(Calendar.YEAR,selectYear);
         endCal.set(Calendar.MONTH,11);
         endCal.set(Calendar.DAY_OF_MONTH,endCal.getActualMaximum(Calendar.DAY_OF_MONTH));
         endCal.set(Calendar.HOUR_OF_DAY,23);
